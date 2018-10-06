f9047b29bf2904b2a74b6a362a574400d03e40dd
Chris Barrett <chris@walrus.cool>
2018-10-06 20:53:18 +1300

    Add a qwerty layer

--- keyboards/atreus/keymaps/walrus/keymap.c
+++ keyboards/atreus/keymaps/walrus/keymap.c
@@ -1,8 +1,9 @@
 #include QMK_KEYBOARD_H
 
 // Layer names
-#define L_LETTER 0
-#define L_SYMBOL 1
+#define L_DVORAK 0
+#define L_QWERTY 1
+#define L_SYMBOL 2
 
 // Custom key identifiers
 #define LTHUMB  LCTL_T(KC_UP)
@@ -21,25 +22,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  '     q     j     k     x       ||         b    m    w    v    z
    *  esc  tab  super shift bspc c/up || m/down spc layer  -    /   enter
    */
-  [L_LETTER] = LAYOUT(
-    M_COLON, KC_COMM, KC_DOT,  KC_P,      KC_Y,                    KC_F,   KC_G,         KC_C,    KC_R,    KC_L,
-    KC_A,    KC_O,    KC_E,    KC_U,      KC_I,                    KC_D,   KC_H,         KC_T,    KC_N,    KC_S,
-    KC_QUOT, KC_Q,    KC_J,    KC_K,      KC_X,                    KC_B,   KC_M,         KC_W,    KC_V,    KC_Z,
-    KC_ESC,  KC_TAB,  KC_LGUI, KC_LSHIFT, KC_BSPC, LTHUMB, RTHUMB, KC_SPC, MO(L_SYMBOL), KC_MINS, KC_SLSH, KC_ENT
+  [L_DVORAK] = LAYOUT(
+    M_COLON, KC_COMM, KC_DOT,  KC_P,    KC_Y,                    KC_F,   KC_G,         KC_C,    KC_R,    KC_L,
+    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                    KC_D,   KC_H,         KC_T,    KC_N,    KC_S,
+    KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                    KC_B,   KC_M,         KC_W,    KC_V,    KC_Z,
+    KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, LTHUMB, RTHUMB, KC_SPC, MO(L_SYMBOL), KC_MINS, KC_SLSH, KC_ENT
   ),
 
-  /* 1: fn with undvorak-ized punctuation
+  /* Qwerty */
+  [L_QWERTY] = LAYOUT(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  LTHUMB, RTHUMB, KC_SPC,  MO(L_SYMBOL), KC_MINS, KC_QUOT, KC_ENT
+  ),
+
+  /* Punctuation
    *
-   *  ~    @      ^     $    %        ||       F4    7     8     9    *
-   *  !    &      {     (    [        ||       F3    4     5     6    +
-   *  \    |      }     )    ]        ||       `     1     2     3    -
-   *     insert super   =  bkspc del  || boot  spc   fn    .     0    #
+   *  ~      @      ^     $    %          ||       F4    7     8     9    *
+   *  !      &      {     (    [          ||       F3    4     5     6    +
+   *  \      |      }     )    ]          ||       `     1     2     3    -
+   *  del   boot  super   =  bkspc QWERTY || DV   spc    fn    .     0    #
    */
   [L_SYMBOL] = LAYOUT(
-    KC_TILD, KC_AT,   KC_CIRC, KC_DOLLAR, KC_PERC,                  KC_F4,   KC_7,    KC_8,   KC_9, KC_ASTR ,
-    KC_EXLM, KC_AMPR, KC_LCBR, KC_LPRN,   KC_LBRC,                  KC_F3,   KC_4,    KC_5,   KC_6, KC_PLUS ,
-    KC_BSLS, KC_PIPE, KC_RCBR, KC_RPRN,   KC_RBRC,                  KC_GRV,  KC_1,    KC_2,   KC_3, KC_MINS ,
-    KC_NO,   KC_INS,  KC_LGUI, KC_EQL,    KC_BSPC, KC_DEL, KC_BOOT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_HASH
+    KC_TILD, KC_AT,   KC_CIRC, KC_DOLLAR, KC_PERC,                    KC_F4,   KC_7,    KC_8,   KC_9, KC_ASTR ,
+    KC_EXLM, KC_AMPR, KC_LCBR, KC_LPRN,   KC_LBRC,                    KC_F3,   KC_4,    KC_5,   KC_6, KC_PLUS ,
+    KC_BSLS, KC_PIPE, KC_RCBR, KC_RPRN,   KC_RBRC,                    KC_GRV,  KC_1,    KC_2,   KC_3, KC_MINS ,
+    KC_DEL,  KC_BOOT, KC_LGUI, KC_EQL,    KC_BSPC, DF(L_QWERTY), DF(L_DVORAK), KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_HASH
   ),
 };
 
