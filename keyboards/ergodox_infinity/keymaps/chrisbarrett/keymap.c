dc842e0ba034661efd2e25a6179f632f64599527
Chris Barrett <chris@walrus.cool>
2020-11-09 01:26:23 +1300

    Right split layer 0

--- keyboards/ergodox_infinity/keymaps/chrisbarrett/keymap.c
+++ keyboards/ergodox_infinity/keymaps/chrisbarrett/keymap.c
@@ -48,14 +48,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              LGUI(LSFT(KC_J)),
                           MT(MOD_LALT, KC_BSPC), KC_UP,      KC_LEFT,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          TT(SYMB),
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+        KC_F7,  KC_F8,   KC_F9,   KC_F10, KC_F11,     KC_F12,           KC_6,
+        KC_TAB, KC_Y,    KC_U,    KC_I,   KC_O,       KC_P,             KC_PIPE,
+                KC_H,    KC_J,    KC_K,   KC_L,       LT(MDIA, KC_SCLN),KC_ENT,
+        KC_EQL, KC_N,    KC_M,    KC_COMM,KC_DOT,     CTL_T(KC_SLSH),   KC_LBRACKET,
+            MT(MOD_RSFT, KC_GRV), KC_QUOT,KC_9,       KC_LBRC,          KC_7,
+
+        LGUI(KC_L),  LGUI(KC_P),
+        LGUI(LSFT(KC_P)),
+        KC_RIGHT,KC_DOWN, MT(MOD_LGUI, KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *
