d9147494ca8516b9a8ef05c27b0946a0aee3ad17
Chris Barrett <chris@walrus.cool>
2020-11-09 00:15:51 +1300

    Left split layer 0

--- keyboards/ergodox_infinity/keymaps/chrisbarrett/keymap.c
+++ keyboards/ergodox_infinity/keymaps/chrisbarrett/keymap.c
@@ -16,36 +16,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   ~    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  | F10  | F11  | F12  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |   `    |   ;  |   ,  |   .  |   p  |   y  | BkTb |           | Tab  |   f  |   g  |   c  |   r  |   l  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | C,Esc  |   a  |   o  |   e  |   u  |   i  |------|           |------|   d  |   h  |   t  |   n  |  s   | Enter  |
+ * |--------+------+------+------+------+------|  &   |           |  #   |------+------+------+------+------+--------|
+ * |   (    |   '  |   q  |   j  |   k  |   x  |      |           |      |   b  |   m  |   w  |   v  |  z   |   @    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |  [   |  {   |  !   |  %   | ^, S |                                       | $, S |  -   |   +  |   /  |  *   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | <Nav | Nav U|       | Nav D| Nav>   |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |Bkspc |      | < Mv |       | Mv > |        | SPC  |
+ *                                 |  /   |  Up  |------|       |------|  Down  |  /   |
+ *                                 | Meta |      | Left |       | Right|        |Hyper |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DEL,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+        KC_TILD,             KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
+        KC_PLUS,             KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,   LSFT(KC_TAB),
+        MT(MOD_LCTL, KC_ESC),KC_A,  KC_S,   KC_D,   KC_F,   KC_G,
+        KC_5,                KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_1,
+                  KC_2,      KC_3,  KC_MINS,KC_EXLM,MT(MOD_LSFT, KC_RCBR),
+
+                                                 LGUI(KC_J), LGUI(KC_K),
+                                                             LGUI(LSFT(KC_J)),
+                          MT(MOD_LALT, KC_BSPC), KC_UP,      KC_LEFT,
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
